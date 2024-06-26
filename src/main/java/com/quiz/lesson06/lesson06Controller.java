18f2c380216c98404a8e829362085d0f5bda4401
syjnk@naver.com <syjnk@naver.com>
2024-06-26 20:57:42 +0900

    lesson06 quiz01_26 06 2024

--- src/main/java/com/quiz/lesson06/lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/lesson06Controller.java
@@ -1,13 +1,18 @@
 package com.quiz.lesson06;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
 
 @RequestMapping("/lesson06")
 @Controller
@@ -23,9 +28,22 @@ public class lesson06Controller {
 	
 	@ResponseBody
 	@PostMapping("/quiz01/add-bookmark")
-	public String addBookmark() {
+	public String addBookmark(
+			@RequestParam("name") String name
+			, @RequestParam("url") String url ) {
+		//DB Insert
+		bookmarkBO.addBookmark(name, url);
 		
+		//AJAX Response String
 		return "success";
 	}
 	
+	@GetMapping("/quiz01/show-List-bookmarks-view")
+	public String showListBookmarks(Model model) {
+		
+		List<Bookmark> listBookmarks = bookmarkBO.getListBookmarks();
+		
+		model.addAttribute("listBookmarks", listBookmarks);
+		return "lesson06/showListBookmarks";
+	}
 }
